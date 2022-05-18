af061baab8431c24f84a15711cb9b1c11d41430b
himash26 <himashdeemantha@gmail.com>
2022-05-18 17:55:10 +0530

    Create agent-v6-ubuntu.sh

---
+++ agent-v6-ubuntu.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+mkdir /home/zabbixagent
+
+cd /home/zabbixagent
+
+##############################################
+# Download & Install zabbix agent for Ubuntu #
+##############################################
+
+wget https://repo.zabbix.com/zabbix/6.1/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.1-1+ubuntu20.04_all.deb
+
+dpkg -i zabbix-release_6.1-1+ubuntu20.04_all.deb
+
+apt update
+
+apt install zabbix-agent -y
+
+cd /etc/zabbix/
+
+rm -fr /home/zabbixagent
+
+################################
+## Configure the zabbix agent ##
+################################
+
+sed -i 's/127.0.0.1/172.19.20.19/g' zabbix_agentd.conf
+
+HOSTNAME=$(cat /etc/hostname)
+
+sed -i 's/Zabbix server/'"$HOSTNAME"'/g' zabbix_agentd.conf
+
+echo "AllowKey=system.run[*]" >> /etc/zabbix/zabbix_agentd.conf
+
+##############################
+## Start & enable the agent ##
+##############################
+
+systemctl restart zabbix-agent
+
+systemctl enable zabbix-agent
+
+echo "INSTALLATION SUCCESSFULL"
